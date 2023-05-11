4fd2295d13e689f8a37c4951a9e963ca1aa51899
lucas.barreto.geodata <lucas.barreto.geodata@gmail.com>
2023-05-11 16:15:47 -0300

    novas adequações, incluindo scroll do card

--- ionic-to-study/src/app/home/home.page.ts
+++ ionic-to-study/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, Inject, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { IonContent } from "@ionic/angular";
 @Component({
     selector: 'app-home',
     templateUrl: './home.page.html',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-    constructor() { }
+    private observer = Inject(IntersectionObserver);
+    private renderer = Inject(Renderer2);
+    
+    @ViewChild('triggerScrollElem', { read: ElementRef, static: true}) triggerScrollElem?: ElementRef;
+    @ViewChild(IonContent, { read: ElementRef, static: true}) ionContentArea?: ElementRef;
+
+    constructor() { 
+
+        console.log('teste');
+    }
 
     ngOnInit() {
+
+        // console.log('this.observer: ', this.observer);
+        this.observer = new IntersectionObserver((entries) => {
+            
+            console.log('entries: ', entries);
+
+            entries.forEach((entry: any) => {
+                console.log('entries: ', entry);
+
+                if(entry.isIntersecting){
+                    // add transform
+                    
+                    this.ionContentArea?.nativeElement.classList.add("no-transform");
+                }else{
+                    // remove transform
+                    // console.log('remove transform: ');
+                    
+                    this.ionContentArea?.nativeElement.classList.remove("no-transform");
+                }
+            })
+        })
+        console.log('this.observer: ', this.observer);
+        this.observer.observe(this.triggerScrollElem?.nativeElement);
     }
 
     handleScroll(event: any){
